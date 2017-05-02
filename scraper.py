a47a17dc90ed37c458f80be2627daac64cf4c107
vagrant <support@codingbrain.com>
2017-05-02 13:17:14 +0100

    scraper skeleton

--- scraper.py
+++ scraper.py
@@ -18,6 +18,8 @@ class Scraper:
         except Exception as e:
             scraping_result.summary = "Could not scrape summary. Reason: %s" % e
 
-        print("Done: %s = title: %s | thumb: %s | summary: %s" % (url, scraping_result.title, scraping_result.thumbnail, scraping_result.summary))
+        print("Done: %s = title: %s | thumb: %s | summary: %s" %
+              (url, scraping_result.title, scraping_result.thumbnail,
+               scraping_result.summary))
 
         return scraping_result
