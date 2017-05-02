59cf0b8481e793702e3cf9c7d59a8a27c3cfe25b
Bernhard Wenzel <bernhard.wenzel@gmail.com>
2017-05-02 12:44:57 +0100

    scraper skeleton

--- scraper.py
+++ scraper.py
@@ -9,22 +9,15 @@ class ScrapingResult:
 
 
 class Scraper:
-
     def scrape(self, url):
         scraping_result = ScrapingResult()
         scraping_result.url = url
         a = Article(url)
         try:
             print("Parsing %s... " % url)
-
-            a.download()
-            a.parse()
-            a.nlp()
-            scraping_result.title = url if not a.title else a.title
-            scraping_result.thumbnail = "100x100.png" if not a.top_image else a.top_image
-            scraping_result.summary = a.summary
         except Exception as e:
             scraping_result.summary = "Could not scrape summary. Reason: %s" % e
 
         print("Done: %s = title: %s | thumb: %s | summary: %s" % (url, scraping_result.title, scraping_result.thumbnail, scraping_result.summary))
+
         return scraping_result
