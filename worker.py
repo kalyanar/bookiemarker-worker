159b4b4e540a75a2942013720a5a521a0bf9e4f8
Bernhard Wenzel <bernhard.wenzel@gmail.com>
2017-05-04 14:58:03 +0100

    exercise created

--- worker.py
+++ worker.py
@@ -2,13 +2,18 @@ import pika
 import json
 from scraper import Scraper
 
+
+def publish_result(scraping_result):
+    j = json.dumps(scraping_result.__dict__)
+    properties = pika.BasicProperties(content_type="application/json")
+
+
+def callback(ch, method, properties, body):
+    url = json.loads(body.decode('utf-8'))['url']
+    scraper = Scraper()
+    result = scraper.scrape(url)
+
 credentials = pika.PlainCredentials("user", "password")
 parameters = pika.ConnectionParameters(host='localhost', port=7234, credentials=credentials)
 
 print('[*] Waiting for tasks. To exit press CTRL+C')
-
-def publish_result(scraping_result):
-    j = json.dumps(scraping_result.__dict__)
-
-def callback(ch, method, properties, body):
-    url = json.loads(body.decode('utf-8'))['url']
